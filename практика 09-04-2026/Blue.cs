7e8dd2215fa94ac204ebce8617676f1be77359ef
AldarKobren <aldarzte@gmail.com>
2026-04-09 21:46:46 +0300

    Add files via upload

---
+++ практика 09-04-2026/Blue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public abstract class Blue : Object
+    {
+        private string _input;
+        public string Input => _input;
+
+        protected static char[] _punc;
+
+        static Blue()
+        {
+            _punc = new char[] { '.', '!', '?', ',', ':', '\"', ';', '–', '(', ')', '[', ']', '{', '}', '/' };
+        }
+
+        protected Blue(string input)
+        {
+            _input = input;
+        }
+        public abstract void Review();
+        public virtual void ChangeText(string input)
+        {
+            _input = input;
+            Review();
+        }
+        protected string[] Getwords(string text)
+        {
+            // .Select(x => x.Trim(_punc))
+            // .Where(x => !String.IsNullOrEmprty(x))
+            // .ToArray()
+            string[] dirtyWords = text.Split(' ');
+            return null;
+        }
+    }
+}
---
+++ практика 09-04-2026/Task1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Task1 : Blue
+    {
+        private int _output;
+        public int Output => _output;
+
+        public Task1(string text) : base(text)
+        {
+            _output = 0;
+            Review();
+        }
+
+        public override void Review()
+        {
+            _output = 134;
+        }
+
+        public override string ToString()
+        {
+            return $"{_input}{Environment.NewLine}{_output}";
+        }
+    }
+}
